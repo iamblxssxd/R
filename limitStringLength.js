52324f5642be43bda70221e06c410c83fd357535
blxssxd <iamblxssxd@gmail.com>
2021-01-17 17:37:39 -0500

    Return a truncated string

---
+++ limitStringLength.js
@@ -0,0 +1,15 @@
+/*
+Complete the solution, so that it returns the truncated version of the string followed by '...'.
+Example:
+solution('Testing String',3) // should return 'Tes...'
+solution('Testing String',8) // should return 'Testing ...'
+solution('Test',8) // should return 'Test'
+*/
+
+function solution(string, limit) {
+  if (limit < string.length) {
+    return string.slice(0, limit).padEnd(limit + 3, ".");
+  } else {
+    return string;
+  }
+}
