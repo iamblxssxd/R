dbf1f2de31186120f824e98315b0a53409048204
blxssxd <iamblxssxd@gmail.com>
2021-04-20 19:09:55 -0400

    Check if a given value and a given type are matching

---
+++ matchingType.js
@@ -0,0 +1,9 @@
+/*
+Write function typeValidation that will accept two parameters: variable and type and check if type of variable is matching type. Return true if types match or false if not.
+
+Examples:
+typeValidation(42, "number");   // => true
+typeValidation("42", "number"); // => false
+*/
+
+let typeValidation = (v, t) => typeof v === t;
--- wilsonPrimes.js
+++ wilsonPrimes.js
@@ -6,17 +6,13 @@ Then ((P-1)! + 1) / (P * P) should give a whole number.
 Your task is to create a function that returns true if the given number is a Wilson prime.
 */
 
-//Answer//
-
 function amIWilson(P) {
-function factorialize(num) {
-  if (num < 0) 
-        return -1;
-  else if (num == 0) 
-      return 1;
-  else {
-      return (num * factorialize(num - 1));
+  function factorialize(num) {
+    if (num < 0) return -1;
+    else if (num == 0) return 1;
+    else {
+      return num * factorialize(num - 1);
+    }
   }
-}
-return !(((factorialize(P-1) + 1)/(P * P))%1)
+  return !(((factorialize(P - 1) + 1) / (P * P)) % 1);
 }
