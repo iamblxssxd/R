0fec5129a037bc0578a7e84f52048cf6e2e9604d
blxssxd <iamblxssxd@gmail.com>
2021-05-18 13:43:35 -0400

    Fix the Syntax for objects

---
+++ fixIdentifiers.js
@@ -0,0 +1,11 @@
+/*
+An identifier is simply a name...
+Can you amend this object so that its properties comprise only vaild identifiers?
+*/
+
+var Person = {
+  name: "John",
+  secondName: "Doe",
+  emailAddress: "john.doe@email.com",
+  male_female: "M",
+};
