64f2dbb00fa038937cba4d0297b7fed466484b99
blxssxd <iamblxssxd@gmail.com>
2021-08-04 15:59:48 -0400

    Fix the function that returns an object

--- fixDevLab.js
+++ fixDevLab.js
@@ -2,8 +2,6 @@
 Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
 */
 
-//Answer//
-
-var a = "dev"
-var b = "Lab"
-var name = a + b
+var a = "dev";
+var b = "Lab";
+var name = a + b;
