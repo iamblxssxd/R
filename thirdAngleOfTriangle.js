cccb71d2fb8282a062c12a55b764de5b31440cda
blxssxd <iamblxssxd@gmail.com>
2021-12-01 15:13:55 -0500

    Calculate the third angle of a triangle

---
+++ thirdAngleOfTriangle.js
@@ -0,0 +1,10 @@
+/*
+You are given two angles (in degrees) of a triangle.
+Write a function to return the 3rd.
+Note: only positive integers will be tested.
+*/
+
+function otherAngle(a, b) {
+  const A = a + b;
+  return 180 - A;
+}
