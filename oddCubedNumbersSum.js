249168295f70c51ca70c4b0960102fb9861854d4
blxssxd <iamblxssxd@gmail.com>
2022-01-25 16:26:38 -0500

    Create oddCubedNumbersSum.js

---
+++ oddCubedNumbersSum.js
@@ -0,0 +1,8 @@
+/*
+Find the sum of the odd numbers within an array, after cubing the initial integers. The function should return undefined/None/nil/NULL if any of the values aren't numbers.
+*/
+
+const cubeOdd = (a) =>
+  a.every((x) => typeof x === "number")
+    ? a.filter((x) => x % 2).reduce((a, b) => a + b ** 3, 0)
+    : undefined;
